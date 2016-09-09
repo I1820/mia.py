502bf24a6a7f934e0a59450545c31d567d8ec1bc
Parham Alvani <parham.alvani@gmail.com>
2016-09-09 21:50:13 +0430

    Improve our Log :)

--- I1820/log/log.py
+++ I1820/log/log.py
@@ -7,19 +7,11 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 import datetime
-import requests
+from .. import i1820_id
 
 
 class I1820Log:
     def __init__(self, data: dict):
         self.states = data
         self.timestamp = datetime.datetime.now()
-        self.endpoint = ""
-
-    def send(self):
-        log = {
-            "timestamp": self.timestamp.timestamp(),
-            "data": {"states": self.states},
-            "endpoint": self.endpoint
-        }
-        requests.post('http://192.168.1.9:1373/log', json=log)
+        self.endpoint = str(i1820_id)
