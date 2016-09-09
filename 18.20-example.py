b7b1e130dbd22fbe07176797dc41d306dd68cd0f
Parham Alvani <parham.alvani@gmail.com>
2016-09-09 22:34:01 +0430

    Sample app was added.

--- 18.20-example.py
+++ 18.20-example.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from I1820.I1820App import I1820App
+from I1820.app import I1820App
 
+app = I1820App('192.168.1.9', 1373)
+
+
+@app.notification('lamp')
+def lamp_notification(data: dict):
+    print(data)
 
 if __name__ == '__main__':
-    app = I1820App('192.168.1.9', 1373)
     app.start()
