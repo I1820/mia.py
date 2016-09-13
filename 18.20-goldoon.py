50adb7fe83dd8c16f3c4b1b67008ad0656278636
Parham Alvani <parham.alvani@gmail.com>
2016-09-13 09:27:09 +0430

    18.20-goldoon sample was added.

---
+++ 18.20-goldoon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import flask
+
+from I1820.app import I1820App
+from I1820 import wapp
+
+app = I1820App('127.0.0.1', 1373)
+
+@wapp.route('/goldoon', methods=['PUT'])
+def goldon_handler():
+    humidity = flask.request.form['humidity']
+    app.log('temperature', '1', {'temperature': humidity})
+    return ""
+
+
+if __name__ == '__main__':
+    app.add_thing('temperature', '0')
+    app.start()
