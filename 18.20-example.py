ee576bbbcea49088a414d829142167a256ce1573
Parham Alvani <parham.alvani@gmail.com>
2016-09-16 02:13:46 +0430

    More data in example :))))

--- 18.20-example.py
+++ 18.20-example.py
@@ -2,6 +2,9 @@
 
 from I1820.app import I1820App
 
+import time
+
+
 app = I1820App('192.168.1.9', 1373)
 
 
@@ -13,4 +16,8 @@ if __name__ == '__main__':
     app.add_thing('lamp', '0')
     app.add_thing('temperature', '1')
     app.start()
-    app.log('temperature', '1', {'temperature': 10})
+    i = 10
+    while True:
+        i = (i + 10) % 100
+        app.log('temperature', '1', {'temperature': i})
+        time.sleep(10)
