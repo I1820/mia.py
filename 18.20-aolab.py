207a1016cb84693361f125deae7589de05eed88f
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 23:11:08 +0430

    AoLab sample was improved.

--- 18.20-aolab.py
+++ 18.20-aolab.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import serial
 import io
-import threading
 import time
 
 from AoLab.protocol.hasht import HashtProtocol
 from I1820.app import I1820App
+from I1820.domain.notif import I1820Notification
 
 app = I1820App('192.168.128.90', 8080, '0.0.0.0', 1373)
 
@@ -13,17 +13,16 @@ ser = serial.serial_for_url('/dev/ttyUSB0', baudrate=9600, timeout=1)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
 
-def send_notification(command):
-    sio.write(command)
+@app.notification('lamp')
+def send_notification(data: I1820Notification):
+    node_id, device_id = data.device_id.split(':')
+    command = '1' if data.settings['on'] else '0'
+
+    time.sleep(5)
+    sio.write(HashtProtocol().marshal(data.type, device_id,
+                                      node_id, command))
     sio.flush()
     ser.flush()
-
-
-def notification():
-    while True:
-        time.sleep(5)
-        send_notification('@1,l11.')
-
 
 if __name__ == '__main__':
     # Temperature
@@ -46,12 +45,12 @@ if __name__ == '__main__':
 
     # Lamp
     app.add_thing('lamp', '1:2')
+
     app.start()
-    threading.Thread(target=notification).start()
     while True:
         line = sio.readline()
         print(line)
-        data = HashtProtocol().handler(line)
+        data = HashtProtocol().unmarshal(line)
         if data is not None:
             for thing in data.things:
                 app.log(thing['type'],
