3c52a7c4cfcbc4ff7373e580fdfa61e804079c94
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 18:32:08 +0430

    Some nasty improvements ....

--- 18.20-aolab.py
+++ 18.20-aolab.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python3
 import serial
 import io
+import threading
+import time
 
 from AoLab.protocol.hasht import HashtProtocol
 from I1820.app import I1820App
 
-app = I1820App('iot.ceit.aut.ac.ir', 58902, '0.0.0.0', 3000)
+app = I1820App('192.168.128.90', 8080, '0.0.0.0', 1373)
+
+ser = serial.serial_for_url('/dev/ttyUSB0', baudrate=9600, timeout=1)
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
+
+def send_notification(command):
+    sio.write(command)
+    sio.flush()
+    ser.flush()
+
+
+def notification():
+    while True:
+        time.sleep(5)
+        send_notification('@1,l11.')
 
 
 if __name__ == '__main__':
-    app.add_thing('temperature', '0')
+    app.add_thing('temperature', '1:1')
+    app.add_thing('temperature', '2:1')
+    app.add_thing('temperature', '3:1')
+    app.add_thing('temperature', '4:1')
+    app.add_thing('humidity', '1:1')
+    app.add_thing('humidity', '2:1')
+    app.add_thing('humidity', '3:1')
+    app.add_thing('humidity', '4:1')
+    app.add_thing('light', '1:1')
+    app.add_thing('light', '2:1')
+    app.add_thing('light', '3:1')
+    app.add_thing('light', '4:1')
+    app.add_thing('lamp', '1:2')
     app.start()
-    ser = serial.serial_for_url('/dev/ttyUSB0', baudrate=9600, timeout=1)
-    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+    threading.Thread(target=notification).start()
     while True:
         line = sio.readline()
         print(line)
