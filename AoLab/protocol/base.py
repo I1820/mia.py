060af7ff6a0554e597f6f08e837998f06381c47b
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 14:51:30 +0430

    Base Protocol was improved.

--- AoLab/protocol/base.py
+++ AoLab/protocol/base.py
@@ -13,5 +13,9 @@ from ..domain.message import AoLabThingMessage
 class AoLabSerialProtocol:
 
     @abc.abstractmethod
+    def write(self, type, device_id, node_id, command):
+        pass
+
+    @abc.abstractmethod
     def handle(self, message: str) -> AoLabThingMessage:
         pass
