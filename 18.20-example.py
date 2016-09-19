e3a2c77b22462d220cee39dd4bce42d12497f53b
Parham Alvani <parham.alvani@gmail.com>
2016-09-19 22:59:06 +0430

    Improve our basic example.

--- 18.20-example.py
+++ 18.20-example.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 from I1820.app import I1820App
+from I1820.domain.notif import I1820Notification
 
 import time
 
 
-app = I1820App('192.168.1.9', 1373)
+app = I1820App('192.168.1.9', 8080)
 
 
 @app.notification('lamp')
-def lamp_notification(data: dict):
+def lamp_notification(data: I1820Notification):
     print(data)
 
 if __name__ == '__main__':
