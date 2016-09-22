c8c0cd19bc0c773f3867979a6ede598c03627f3b
Parham Alvani <parham.alvani@gmail.com>
2016-09-22 23:07:53 +0330

    Lamp test script was improved.

--- lamp.sh
+++ lamp.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 # In The Name Of God
 # ========================================
-# [] File Name : khar.sh
+# [] File Name : lamp.sh
 #
 # [] Creation Date : 21-09-2016
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-for i in `seq 1 10`; do
+
+for i in `seq 1 8`; do
 	curl -X PUT -H "Content-Type: application/json" -d "{
 		\"type\": \"lamp\",
 		\"rpi_id\": \"b07882d6-5c28-597b-89f9-d250f74b0bad\",
-	      	\"device_id\": \"1:$i\",
+	      	\"device_id\": \"1:5\",
 	      	\"settings\": {
 			\"on\": true
+		}
+	}" "iot.ceit.aut.ac.ir:58902/thing"
+	curl -X PUT -H "Content-Type: application/json" -d "{
+		\"type\": \"lamp\",
+		\"rpi_id\": \"b07882d6-5c28-597b-89f9-d250f74b0bad\",
+	      	\"device_id\": \"1:5\",
+	      	\"settings\": {
+			\"on\": false
 		}
 	}" "iot.ceit.aut.ac.ir:58902/thing"
 done
