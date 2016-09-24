1bc144a988bbfc2657d5ae1355f4ee0685c3cdca
Parham Alvani <parham.alvani@gmail.com>
2016-09-24 16:33:22 +0330

    Zigbee connection sample was improved.

--- 18.20-zigbee.py
+++ 18.20-zigbee.py
@@ -2,17 +2,23 @@
 import serial
 from xbee import ZigBee
 from I1820.app import I1820App
-from I1820.log.log import I1820Log
 
-app = I1820App('192.168.1.9', 1373)
+app = I1820App('192.168.128.90', 8080, '0.0.0.0', 1820)
 
-app.add_thing('temprature', '0')
 
 if __name__ == '__main__':
-    serial_port = serial.Serial('COM3', 9600)
+    serial_port = serial.Serial('/dev/ttyUSB1', 9600)
     xbee = ZigBee(serial_port)
+    app.add_thing('multisensor', '1')
     app.start()
     while True:
         frame = xbee.wait_read_frame()
-        log = I1820Log()
-        app.log(log)
+        print(frame)
+        sample = frame['samples'][0]
+        humidity = str(sample['adc-2'])
+        temperature = str(sample['adc-1'])
+        light = str(sample['adc-0'])
+        app.log('multisensor', '1', {'humidity': humidity,
+                                     'temperature': temperature,
+                                     'light': light}
+                )
