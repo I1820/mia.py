38aae2592dda001bc409867761450a752c10850d
Parham Alvani <parham.alvani@gmail.com>
2016-09-25 04:22:00 +0330

    Better identification generating.

--- I1820/__init__.py
+++ I1820/__init__.py
@@ -2,4 +2,5 @@ import uuid
 from flask import Flask
 
 wapp = Flask("I1820-Plug")
-i1820_id = uuid.uuid5(uuid.NAMESPACE_DNS, 'I1820.aolab.ceit.aut.ac.ir')
+i1820_id = uuid.uuid5(uuid.NAMESPACE_URL,
+                      'I1820://%s.aolab.ceit.aut.ac.ir' % uuid.getnode())
