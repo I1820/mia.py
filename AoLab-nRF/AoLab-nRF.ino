0fd6a7f108f55f9c6041e997f83896157c04c434
Parham Alvani <parham.alvani@gmail.com>
2016-09-27 18:07:16 +0330

    New arduino nrf plug was added.

--- AoLab-nRF/AoLab-nRF.ino
+++ AoLab-nRF/AoLab-nRF.ino
@@ -2,27 +2,36 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include "printf.h"
+#include <elapsedMillis.h>
+#define interval 10
 
+elapsedMillis stopwatch;
 RF24 radio(9, 10);
-/* Radio pipe addresses for the 6 nodes to communicate. */
+int count=0;
+boolean flag = false;
 const uint64_t pipes[6] = { 0xE7D3F03577,
 	0xC2C2C2C2C2,
 	0xC2C2C2C2C3,
 	0xC2C2C2C2C4,
 	0xC2C2C2C2C5,
 	0xC2C2C2C2C6
-};
+};   // Radio pipe addresses for the 6 nodes to communicate.
+byte node_address=1;
+void ack(char text[]);
+String out_char="";
+String c="";
+char charbuff[50];
+char charbuff1[50];
 
 void setup()
 {
 	while (!Serial);
 	Serial.begin(9600);
 	printf_begin();
-
-	/* nRF startup :) */
 	radio.begin();
-	radio.setPALevel(RF24_PA_MAX);
-	radio.setRetries(10, 5);
+	radio.setPALevel( RF24_PA_MAX ) ;
+	radio.setRetries(10,5);
+	radio.setChannel(108);
 	radio.openReadingPipe(0, pipes[0]);
 	radio.openReadingPipe(0, pipes[1]);
 	radio.openReadingPipe(0, pipes[2]);
@@ -32,56 +41,94 @@ void setup()
 	radio.startListening();
 	radio.printDetails();
 }
-
 void loop()
 {
 	radio.startListening();
 	radio.openReadingPipe(0, pipes[0]);
-	radio.openReadingPipe(0, pipes[1]);
-	radio.openReadingPipe(0, pipes[2]);
-	radio.openReadingPipe(0, pipes[3]);
-	radio.openReadingPipe(0, pipes[4]);
+
 	radio.openReadingPipe(1, pipes[5]);
-
-	if (radio.available()) {
-		char ingress[33] = {0};
-		radio.read(&ingress, sizeof(ingress));
-
-		/* Send ingress information to serial */
-		Serial.println(ingress);
-
-		/* Send back the acknowledgement */
-		radio.openWritingPipe(pipes[0]);
-		radio.stopListening();
-		delayMicroseconds(200);
-		if (ingress[0] == '@') {
-			for(int i = 0; i < 2; i++){
-				char ack[5] = "ack";
-				ack[3] = ingress[1];
-				ack[4] = 0;
-				radio.write(ack, 4);
+	if (radio.available())
+	{
+		char text[33] = {0};
+		radio.read(&text, 33);
+		Serial.println(text);
+		switch (text[1]) {
+			case '1':
+				radio.openWritingPipe(pipes[0]);
+				radio.stopListening();
 				delayMicroseconds(200);
-			}
+				for(int i=0;i<2;i++){
+					radio.write("ack1",sizeof("ack1"));
+					delayMicroseconds(200);
+				}
+				break;
+			case '2':
+				radio.openWritingPipe(pipes[0]);
+				radio.stopListening();
+				delayMicroseconds(200);
+				for(int i=0;i<2;i++){
+					radio.write("ack2",sizeof("ack2"));
+					delayMicroseconds(200);
+				}
+				break;
+			case '3':
+				radio.openWritingPipe(pipes[0]);
+				radio.stopListening();
+				delayMicroseconds(200);
+				for(int i=0;i<2;i++){
+					radio.write("ack3",sizeof("ack3"));
+					delayMicroseconds(300);
+				}
+				break;
+			case '4':
+				radio.openWritingPipe(pipes[0]);
+				radio.stopListening();
+				delayMicroseconds(200);
+				for(int i=0;i<2;i++){
+					radio.write("ack4",sizeof("ack4"));
+					delayMicroseconds(300);
+				}
+				break;
+			case '9':
+				radio.openWritingPipe(pipes[0]);
+				radio.stopListening();
+				delayMicroseconds(200);
+				for(int i=0;i<2;i++){
+					radio.write("ack9",sizeof("ack9"));
+					delayMicroseconds(300);
+				}
+				break;
 		}
 	}
-
-	if (Serial.available())
+	int index1=0;
+	if ( Serial.available() )
 	{
-		String input = Serial.readString();
-		int index = 0;
-		char charbuff[32];
-		do {
-			Serial.println("s" + input);
-			index = input.indexOf('.');
-			if (index != -1 && index < 31) {
-				String egress = input.substring(0, index + 1);
-				Serial.println("r" + egress);
-				egress.toCharArray(charbuff, 32);
-				radio.openWritingPipe(pipes[5]);
-				radio.stopListening();
-				radio.write(charbuff, index + 1);
-				input = input.substring(index + 1);
+		c = Serial.readString();
+		for(int i=0;i<32;i++){
+			if(c[i]=='.')
+				index1=i+1;
+		}
+		c.toCharArray(charbuff1, index1+1);
+		radio.openWritingPipe(pipes[5]);
+		radio.stopListening();
+		radio.write(charbuff1,index1);
+		flag=false;
+		stopwatch=0; //stopwatch reset
+		while(interval>stopwatch && flag==false)
+		{
+			if (radio.available())
+			{
+				char text[33] = {0};
+				radio.read(&text, 33);
+				Serial.println("recived:");
+				Serial.println(text);
+				int result=strcmp(text, "set1");
+				if(result==0)
+				{
+					flag=true;//out_char="";
+					break;
+				}
 			}
-		} while(index != -1 && input.length() > 0);
+		}
 	}
 }
