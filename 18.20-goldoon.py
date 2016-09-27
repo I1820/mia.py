868d2be75718b3e6643fd04440b1f79b2963b9a0
Parham Alvani <parham.alvani@gmail.com>
2016-09-27 17:57:47 +0330

    Goldoon example was improved.

--- 18.20-goldoon.py
+++ 18.20-goldoon.py
@@ -4,7 +4,7 @@ import flask
 from I1820.app import I1820App
 from I1820 import wapp
 
-app = I1820App('127.0.0.1', 1373, '0.0.0.0', 3000)
+app = I1820App('127.0.0.1', 8080, '0.0.0.0', 3000)
 
 
 @wapp.route('/goldoon', methods=['PUT'])
@@ -17,3 +17,5 @@ def goldon_handler():
 if __name__ == '__main__':
     app.add_thing('temperature', '0')
     app.start()
+    while True:
+        pass
