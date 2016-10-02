55163a9b0d7196d85009e8b199ad4f3a7b657a0c
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-02 22:37:44 +0330

    Adds blinking bash script

--- lamp.sh
+++ lamp.sh
@@ -5,15 +5,22 @@
 #
 # [] Creation Date : 21-09-2016
 #
+# [] Improved By : Iman Tabrizian (tabrizian@outlook.com)
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+status=true
 for i in `seq 1 9`; do
 	curl -X PUT -H "Content-Type: application/json" -d "{
 		\"type\": \"lamp\",
 		\"rpi_id\": \"066156d8-df62-5894-809b-d51ec5a2ff3d\",
 		\"device_id\": \"1:$i\",
 		\"settings\": {
-			\"on\": true
+			\"on\": $status
 		}
-	}" "192.168.128.90:8080/thing"	
+	}" "192.168.128.90:8080/thing"
+  if ["$status" = "true"]; then
+    status=false
+  else
+    status=true
+  fi
 done
