2c72f94cb451486f75bb7687c5173b87ddf14aad
Parham Alvani <parham.alvani@gmail.com>
2016-10-05 01:24:55 +0330

    18.20-presenter was improved.

--- 18.20-presenter.py
+++ 18.20-presenter.py
@@ -15,19 +15,19 @@ def open_slide_handler(path):
 
 @wapp.route('/next', methods=['GET'])
 def next_slide_handler():
-    call(["./next.sh", "Page_Down"])
+    call(["./AoLab-Presenter/next.sh", "Page_Down"])
     return ""
 
 
 @wapp.route('/back', methods=['GET'])
 def back_slide_handler():
-    call(["./next.sh", "Page_Up"])
+    call(["./AoLab-Presenter/next.sh", "Page_Up"])
     return ""
 
 
 @wapp.route('/quit', methods=['GET'])
 def quit_slide_handler():
-    call(["./next.sh", "q"])
+    call(["./AoLab-Presenter/next.sh", "q"])
     return ""
 
 if __name__ == '__main__':
