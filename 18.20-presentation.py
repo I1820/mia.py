a1641a30ecd0a9dc98e4aca87ce19022aeb51e30
Parham Alvani <parham.alvani@gmail.com>
2016-10-04 14:12:21 +0330

    Presentor script was improved.

--- 18.20-presentation.py
+++ 18.20-presentation.py
@@ -4,12 +4,12 @@ from subprocess import call
 from I1820.app import I1820App
 from I1820 import wapp
 
-app = I1820App('127.0.0.1', 8080, '0.0.0.0', 3000)
+app = I1820App('192.168.128.90', 8080, '0.0.0.0', 1820)
 
 
 @wapp.route('/next', methods=['GET'])
 def next_slide_handler():
-    call(["for w in `xdotool search LibreOffice` ; do xdotool key --window $w Page_Down ; done"])
+    call(["./next.sh"])
     return ""
 
 
