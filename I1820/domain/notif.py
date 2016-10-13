2a6bbd96e74d2a6b58089b52c1478c5b3ae3133f
Parham Alvani <parham.alvani@gmail.com>
2016-10-13 03:44:53 +0330

    New I1820Notification bson model was added.

--- I1820/domain/notif.py
+++ I1820/domain/notif.py
@@ -6,10 +6,22 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-import json
+import bson
 
 
-class I1820Notification:
+class I1820Notification(bson.BSONCoding):
+    '''
+    The I1820Notification object contains information that is used to
+    send notification into end devices.
+    :param type: type of target end device.
+    :type type: str
+    :param device: identification of target end device.
+    :type device: str
+    :param settings: configurations wanted to apply on target end device.
+    :type settings: dict
+    :param endpoint: identification of target end device Raspberry PI.
+    :type endpoint: str
+    '''
     def __init__(self, type: str, device: str,
                  settings: dict,
                  endpoint: str):
@@ -18,25 +30,18 @@ class I1820Notification:
         self.settings = settings
         self.endpoint = endpoint
 
-
-class I1820NotificationJSONEncoder(json.JSONEncoder):
-    def default(self, obj: I1820Notification):
-        if isinstance(obj, I1820Notification):
+    def bson_encode(self):
             return {
-                'type': obj.type,
-                'device': obj.device,
-                'settings': obj.settings,
-                'endpoint': obj.endpoint
+                'type': self.type,
+                'device': self.device,
+                'settings': self.settings,
+                'endpoint': self.endpoint
             }
-        else:
-            raise TypeError(
-                ("I1820NotificationEncoder got"
-                 " {} instead of I1820Notification").format(
-                    type(obj)))
 
+    def bson_init(self, raw_values):
+        self.type = raw_values['type']
+        self.device = raw_values['device']
+        self.settings = raw_values['settings']
+        self.endpoint = raw_values['endpoint']
 
-class I1820NotificationDictDecoder:
-    @staticmethod
-    def decode(obj: dict) -> I1820Notification:
-        return I1820Notification(obj['type'], obj['device'],
-                                 obj['settings'], obj['endpoint'])
+bson.import_class(I1820Notification)
