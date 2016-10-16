0d94e852058c6ceee24b7a4a8bed54e55d6f4484
Parham Alvani <parham.alvani@gmail.com>
2016-10-16 23:20:55 +0330

    TaaS sample was added :)

---
+++ 18.20-taas.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import serial
+import io
+
+from AoLab.protocol.hasht import HashtProtocol
+from I1820.app import I1820App
+from I1820.domain.notif import I1820Notification
+
+
+token = '83DB8F6299E0A303730B5F913B6A3DF420EBC2C2'
+
+app = I1820App(token, 'iot.ceit.aut.ac.ir', 58904)
+
+
+ser = serial.serial_for_url('/dev/ttyUSB0', baudrate=9600, timeout=1)
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
+
+@app.notification('lamp')
+def lamp_notification(data: I1820Notification):
+    node_id, device_id = data.device.split(':')
+    command = '1' if data.settings['on'] else '0'
+
+    command_raw = HashtProtocol().marshal(data.type, device_id,
+                                          node_id, command)
+
+    serial_write(command_raw)
+
+
+def serial_write(command):
+    sio.write(command)
+    sio.flush()
+
+
+def serial_read():
+    line = sio.readline()
+    if len(line) != 0:
+        print(line)
+    data = HashtProtocol().unmarshal(line)
+    if data is not None:
+        states = {}
+        for thing in data.things:
+            states[thing['type']] = thing['value']
+        app.log('multisensor', data.node_id, states)
+
+if __name__ == '__main__':
+    # MultiSensors
+    app.add_thing('multisensor', '8')
+    app.add_thing('lamp', '2:1')
+
+    app.run()
+    while True:
+        serial_read()
