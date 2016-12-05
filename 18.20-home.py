d260717b42daf0cbf5ca3485eb870e3098772c2b
Parham Alvani <parham.alvani@gmail.com>
2016-12-05 19:14:29 +0330

    Home example was improved.

--- 18.20-home.py
+++ 18.20-home.py
@@ -22,9 +22,27 @@ logger = logging.getLogger('I1820.home')
 @app.notification('lamp')
 def lamp_notification(data: I1820Notification):
     print(data)
-
     node_id, device_id = data.device.split(':')
-    command = '1' if data.settings['on'] else '0'
+    if 'on' in data.settings:
+        command = '1' if data.settings['on'] else '0'
+    else:
+        return
+
+    command_raw = HashtProtocol().marshal(data.type, device_id,
+                                          node_id, command)
+
+    serial_write(command_raw)
+
+
+@app.notification('cooler')
+def cooler_notification(data: I1820Notification):
+    node_id, device_id = data.device.split(':')
+    if 'on' in data.settings:
+        command = '1' if data.settings['on'] else '0'
+    elif 'temperature' in data.settings:
+        command = str(data.settings['temperature'])
+    else:
+        return
 
     command_raw = HashtProtocol().marshal(data.type, device_id,
                                           node_id, command)
@@ -52,8 +70,13 @@ def serial_read():
 if __name__ == '__main__':
     # MultiSensors
     app.add_thing('multisensor', '2')
-    app.add_thing('lamp', '1:1')
-    app.add_thing('lamp', '1:2')
+
+    # Coolers
+    app.add_thing('cooler', '1:1')
+
+    # Lamps
+    for i in range(1, 10):
+        app.add_thing('lamp', '%d:%d' % (1, i))
 
     app.run()
     while True:
