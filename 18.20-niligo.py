c5436492c239e848633a76d0e4c3013a8bd102b6
mahtab farrokh <mahtab.farrokh@yahoo.com>
2017-01-31 13:07:03 +0330

    niligo set notif-pro

--- 18.20-niligo.py
+++ 18.20-niligo.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 import logging
 import requests
+from requests.auth import HTTPDigestAuth
 
 from I1820.app import I1820App
 from I1820.domain.notif import I1820Notification
 
 token = '83DB8F6299E0A303730B5F913B6A3DF420EBC2C2'
 
-app = I1820App(token, 'iot.ceit.aut.ac.ir', 58904)
-
+app = I1820App(token, '127.0.0.1')
+url = 'http://192.168.100.1/api/state'
+username = 'admin'
+password = 'admin'
 
 @app.notification('smartLamp')
 def notif (jnotif: I1820Notification) :
     for setting in jnotif.settings :
         if setting['name'] == 'on':
             if setting['value']:
-                pass
+                post_data = {'power': '1'}
+                requests.get(url, data=post_data ,auth=HTTPDigestAuth(username, password))
+                print("on")
             else:
-                pass
-
-
+                post_data = {'power': '0'}
+                r = requests.get(url, data=post_data , auth=HTTPDigestAuth(username, password))
+                print(r.content)
+                print("off")
 
 
 if __name__ == '__main__':
