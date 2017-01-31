bab41068b3d5c7e927398552ad8491f4698b823c
mahtab farrokh <mahtab.farrokh@yahoo.com>
2017-01-31 12:30:24 +0330

    niligo set notif

--- 18.20-niligo.py
+++ 18.20-niligo.py
@@ -2,37 +2,27 @@
 import logging
 import requests
 
-from netdisco.discovery import NetworkDiscovery
 from I1820.app import I1820App
 from I1820.domain.notif import I1820Notification
-
 
 token = '83DB8F6299E0A303730B5F913B6A3DF420EBC2C2'
 
 app = I1820App(token, 'iot.ceit.aut.ac.ir', 58904)
 
-logger = logging.getLogger('I1820.niligo')
-
 
 @app.notification('smartLamp')
-def itunes_notification(data: I1820Notification):
-    for setting in data.settings:
+def notif (jnotif: I1820Notification) :
+    for setting in jnotif.settings :
         if setting['name'] == 'on':
             if setting['value']:
-                requests.get('http://<ip>/api/state?power=1')
+                pass
             else:
-                requests.get('http://<ip>/api/state?power=0')
+                pass
+
+
+
 
 if __name__ == '__main__':
-    netdis = NetworkDiscovery()
-
-    netdis.scan()
-
-    for dev in netdis.discover():
-        print(dev, netdis.get_info(dev))
-
-        netdis.stop()
-
     app.add_thing('smartLamp', '1')
 
     app.run()
